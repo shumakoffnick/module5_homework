7f88884724531e788552bd1ccf841a798e8a7c51
Name <email@example.ru>
2021-11-20 14:23:39 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,177 @@
+
+
+
+
+
+/*Задача 8
+let myMap = {
+
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 'nikolai'
+}
+console.log(myMap);
+
+
+
+
+/* Задача 5
+let x = [1, 2, 3, 4, 5];
+console.log(x.length);
+for(let i = 0; i < x.length; i++){
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+/* Задача 2
+let x = prompt('Введите число'); 
+if(x = Number(x)){console.log('Это число')
+}else if(x = String(x)){console.log('Это строка')
+}else if(x = Boolean(x)){console.log('Это логический тип')
+}else{console.log('Тип не определен')};
+
+
+
+
+
+
+
+/* Задача 1 
+let a = prompt('Введите число');
+let c = +a;
+console.log(typeof(c));
+if(c % 2 === 0){console.log('Число четное')
+}else if(c % 1 === 0){console.log('Число не четное')
+}else if(c = Number(c)){console.log('Это минус')}
+
+
+
+
+
+
+
+
+/* пример
+let x = prompt('Сколько тебе лет?');
+
+if(x = Number(x)){console.log('Отлично')
+}else{console.log('нет')}
+
+
+/* 
+let x = '1';
+
+x = Boolean(x); 
+
+console.log(typeof x);
+console.log(x);
+
+
+/* Задание 7
+
+let x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for( let i = 2; i <= 10; i += 2 ){
+if(i !== x){console.log(i.lehgth)}    
+}
+for( let i = 1; i <= 10; i += 2 ){
+if(i !== x){console.log(i.lehgth)}    
+}
+
+/*
+
+
+
+
+
+
+
+/* Задание 6 
+let arr = [1, 2, 3, 4, 5, 5];
+let arr1 = [...new Set(arr)];
+let arr2 = arr1 !== arr;
+console.log(arr2);
+
+/*
+
+/*
+
+
+
+
+
+
+
+/* Задание 4 
+
+let number = Math.random() * 100;
+number = Math.round(number);
+console.log(number);
+
+/*
+
+
+/* Задание 3
+
+let x = 'Hello';
+let y = x.split('');
+
+console.log(y.reverse().join(''));
+
+
+/*
+
+/* Задание 5.4
+
+x = '';
+console.log(typeof x);
+a = x == '';
+console.log(a || x)
+if(a || x == ''){console.log('Да, это так')
+}else{'Тип X не определен'};
+
+x = +'';
+console.log(typeof x);
+a = x == +'';
+console.log(a || x)
+if(a || x == +''){console.log('Да, это так')
+}else{'Тип X не определен'};
+
+x = '' == '';
+console.log(typeof x);
+a = x == '';
+console.log(a || x)
+if(a || x == ''){console.log('Да, это так')
+}else{'Тип X не определен'};
+                                */
+
+/* Задание 1 (5.3)
+a = prompt('');
+console.log(typeof +a);
+if(a % 2 === 0){console.log(a)}
+else if (a % 1 === 0){console.log(a)}
+else if (String){console.log('Упс, кажется, вы ошиблись')} */
+
+
+
+
+
+
+/* a = prompt('Введите число');
+if(a % 2 === 0){alert('Число четное')
+}else {alert('Число не четное')} */
+
+/*b = prompt('');
+
+console.log(typeof +b);
+console.log(b || 0); */
